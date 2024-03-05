3e8aee885f5500eb9ba425b35ea4160671f63652
邱婷 <www86527@gmail.com>
2024-03-05 21:26:34 +0800

    作業

--- .sql/作業where.sql
+++ .sql/作業where.sql
@@ -1,5 +1,20 @@
-SELECT *
-From customer;
+select * from customer
+WHERE first_name='Jamie'
 
-SELECT *
-first_name AS Jamie;
+select * from customer
+WHERE first_name='Jamie'AND last_name='Rice'
+
+select * from customer
+WHERE first_name='Adam'OR last_name='Rodriquez'
+
+select * from customer
+WHERE first_name IN('Ann','Anne','Annie')
+
+select * from customer
+WHERE first_name LIKE('Ann%')
+
+select * from customer
+WHERE first_name LIKE('A%') AND LENGTH(first_name) BETWEEN 3 AND 5;
+
+select * from customer
+WHERE first_name LIKE('Bra%') AND NOT last_name='Motley';
